f15a693a1a89545fcb101788abed3875ce1892a6
wangjun <1693841903@qq.com>
2018-08-07 10:51:41 +0800

    基础 server与客服端的交互

--- week1/day1/jdServer.py
+++ week1/day1/jdServer.py
@@ -7,7 +7,7 @@
 @file: jdServer.py
 @version: 1.0
 @time: 2018/8/7 10:24
-@desc:
+@desc: 模拟jd的服务端
 # code is far away from bugs with the god animal protecting
     I love animals. They taste delicious.
              ┏┓     ┏┓
@@ -27,3 +27,23 @@
 import socket
 
 
+def main():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('127.0.0.1', 8089))
+    sock.listen(5)
+
+    while True:
+        connection, address = sock.accept()
+        buf = connection.recv(1024)
+        print(buf.decode('utf8'))
+        connection.sendall(bytes("HTTP/1.1 201 OK\r\n\r\n", "utf8"))
+        # connection.sendall(bytes("<h1>hello world</h1>", "utf8"))
+        with open('hello.html', 'rb') as f:
+            data = f.read()
+        connection.sendall(data)
+        connection.close()
+
+
+if __name__ == '__main__':
+    main()
